14aecb8ae8f50699c264adf104a0dde828a56aef
Sirenitix <suleevn@gmail.com>
2021-09-09 12:27:34 +0600

    Initial commit

---
+++ src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+}
